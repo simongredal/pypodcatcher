95d441ce2266f7c4e75c355f89af06550f0107f8
Simon Gredal <SimonGr3dal@icloud.com>
2020-03-18 20:59:54 +0100

    Implement episode-limit, un-nest some of the code
    The CLI flag for episode-limit has been implemented, useful.
    Changed the for and if statements to do more "bail out early" stuff to make the logic more clear and un-nest the the code.

--- pypodcatcher.py
+++ pypodcatcher.py
@@ -20,17 +20,19 @@ parser.add_argument('-s', '--silent', action='store_true', help='Silences log ou
 parser.add_argument('-l', '--log', nargs=1, help='Saves log output to file.')
 args = parser.parse_args()
 
-# Ugly-ass hack for getting the file extension without trying to regex through a url
+# Ugly-ass hack for getting the file extension without having to regex through a url
 file_extensions = {'audio/mpeg': '.mp3'}
 
+# Remove characters that doesn't play well with 'nix and mac filesystems.'
 def sanitize_filename(filename):
 	return filename.replace('/', '-').replace(':', '')
 
-def logger(message):
+# Small logger.
+def logger(indent, message):
 	if not args.silent:
-		print(message)
+		print('\t'*indent + message)
 
-# Get podcast feeds from OPML
+# Get podcast feeds from OPML file or URL
 parsed_opml = opml.parse(args.OPML)
 
 # Setup current working directory.
@@ -55,53 +57,70 @@ urllib3.disable_warnings()
 for index, opml_outline in enumerate(parsed_opml):
 	rss_feed = feedparser.parse(opml_outline.xmlUrl)
 	
-	logger(f'Downloading podcast {index+1}/{len(parsed_opml)}: {rss_feed.feed.title} with {len(rss_feed.entries)} entries.')
+	logger(1, f'Downloading podcast {index+1}/{len(parsed_opml)}: {rss_feed.feed.title} with {len(rss_feed.entries)} entries.')
 	
 	# Make the directory for the podcast
 	# https://stackoverflow.com/questions/12517451/automatically-creating-directories-with-file-output
 	os.chdir(download_dir)
 	os.makedirs(rss_feed.feed.title, exist_ok=True)
 	os.chdir(rss_feed.feed.title)
-	
+
+	# Loop through each episode in the RSS feed
 	for index, entry in enumerate(rss_feed.entries):
+	# Exit episode loop early if episode-limit is set and has been reached.
+		if (args.limit_episodes is not None) and (index >= args.limit_episodes[0]):
+			continue
+	# TODO: Exit episode loop early if days-limit is set and has been reached.
+		# if (args.limit_days is not None) and 
+
+		# Go to next episode if there are no enclosures in the episode.
+		if len(entry.enclosures) == 0:
+		    logger(2, f'No enclosure found in RSS entry, skipping {index+1}/{len(rss_feed.entries)}')
+		    continue
+		    
 		year = f'{entry.published_parsed[0]:02}'
 		month = f'{entry.published_parsed[1]:02}'
 		day = f'{entry.published_parsed[2]:02}'
 		date=f'{year}-{month}-{day}'
 		audio_filename = f'{date} | {entry.title}'
-		
-		if len(entry.enclosures) > 0:
-			try:
-				enclosure_url = entry.enclosures[0].href
-			except AttributeError:
-				logger(f'No href found in RSS enclosure, skipping {index+1}/{len(rss_feed.entries)}')
-				continue
+
+		# Get the url of the enclosed audio file
+		try:
+			enclosure_url = entry.enclosures[0].href
+		except AttributeError:
+			logger(2, f'No href found in RSS enclosure, skipping {index+1}/{len(rss_feed.entries)}')
+			continue
 			
-			try:
-				file_extension = file_extensions.get(entry.enclosures[0].type, '')
-			except AttributeError:
-				pass
-				
-			if file_extension != '':
+		# Find the Content-Type type in the RSS enclosure
+		try:
+			file_extension = file_extensions.get(entry.enclosures[0].type, '')
+		except AttributeError:
+			pass
+
+		# If the Content-Type couldn't be found in the enclosure, get it when downloading
+		if file_extension == '':
+			with http.request('GET', enclosure_url, preload_content=False, ) as r:
+				file_extension = file_extensions.get(r.headers['Content-Type'], '')
 				filename = sanitize_filename(audio_filename+file_extension)
-			
+
+				# Skip download if the file already exists		
 				if os.path.exists(filename):
-					logger(f'File already exists, skipping {index+1}/{len(rss_feed.entries)}: {filename}')
-				else:
-					with http.request('GET', enclosure_url, preload_content=False, ) as r, open(filename, 'wb') as audio_file:
-						logger(f'Downloading {index+1}/{len(rss_feed.entries)}: {filename}')
-						shutil.copyfileobj(r, audio_file)
-			else:
-				with http.request('GET', enclosure_url, preload_content=False, ) as r:
-					file_extension = file_extensions.get(r.headers['Content-Type'], '')
-					filename = sanitize_filename(audio_filename+file_extension)
+					logger(2, f'File already exists, skipping {index+1}/{len(rss_feed.entries)}: {filename}')
+					continue
 					
-					if os.path.exists(filename):
-						logger(f'File already exists, skipping {index+1}/{len(rss_feed.entries)}: {filename}')
-					else:
-						with open(filename, 'wb') as audio_file:
-							logger(f'Downloading {index+1}/{len(rss_feed.entries)}: {filename}')
-							shutil.copyfileobj(r, audio_file)
-			
+				with open(filename, 'wb') as audio_file:
+					logger(2, f'Downloading {index+1}/{len(rss_feed.entries)}: {filename}')
+					shutil.copyfileobj(r, audio_file)
+
+
 		else:
-			logger(f'No enclosure found in RSS entry, skipping {index+1}/{len(rss_feed.entries)}')
+			filename = sanitize_filename(audio_filename+file_extension)
+
+			# Skip download if the file already exists 		
+			if os.path.exists(filename):
+				logger(2, f'File already exists, skipping {index+1}/{len(rss_feed.entries)}: {filename}')
+				continue
+				
+			with http.request('GET', enclosure_url, preload_content=False, ) as r, open(filename, 'wb') as audio_file:
+				logger(2, f'Downloading {index+1}/{len(rss_feed.entries)}: {filename}')
+				shutil.copyfileobj(r, audio_file)

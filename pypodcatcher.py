9e47fad87274f5af07d5bde4e9d2cdca5c2e8d89
Simon Gredal <SimonGr3dal@icloud.com>
2020-07-22 05:58:18 +0200

    Handle case-sensitivity issues on opml attributes
    I discovered that the opml-module takes the attributes in the opml-file as-is.
    This means that the attributes on the opml-outline are case sensitive, this is not good because opml-files are formated like snowflakes.
    The code now accounts for this handles the keys in a case-insensitive way.

--- pypodcatcher.py
+++ pypodcatcher.py
@@ -56,8 +56,73 @@ urllib3.disable_warnings()
 
 # Go through each feed
 for index, opml_outline in enumerate(parsed_opml):
-	#Read the RSS Feed
-	rss_feed = feedparser.parse(opml_outline.xmlUrl)
+	#!bin/python3
+
+import feedparser, opml, urllib3
+import os, shutil, argparse
+
+
+
+# Setup command line arguments and description, etc.
+parser =  argparse.ArgumentParser(description='Small utility to download complete podcasts from OPML files.')
+parser.add_argument('OPML', help='Path or URL to OPML file with podcasts.')
+parser.add_argument('-d', '--dir', nargs='?', help='Directory to save downloads in. Default is current directory')
+
+parser.add_argument('--limit-days', metavar='N', nargs=1, type=int, help='Limit to downloading episodes newer than N days old. (Not implemented)')
+parser.add_argument('--limit-episodes', metavar='N', nargs=1, type=int, help='Limit to downloading the N newest episodes.')
+parser.add_argument('--reverse', action='store_true', help='Go through the entries in the RSS feeds en reverse order. (i.e. from old to new)')
+
+parser.add_argument('--delete-old', action='store_true', help="Deletes any episode that does not fall within the limit parameters.")
+parser.add_argument('-r', '--reset', action='store_true', help='Deletes contents of the download directory before downloading.')
+
+parser.add_argument('-s', '--silent', action='store_true', help='Silences log output to sceen.')
+parser.add_argument('-l', '--log', nargs=1, help='Saves log output to file.')
+args = parser.parse_args()
+
+# Ugly-ass hack for getting the file extension without having to regex through a url
+file_extensions = {'audio/mpeg': '.mp3', 'audio/x-m4a': '.m4a', 'audio/mpeg4-generic': '.mp4', 'audio/mp4': '.mp4', 'audio/ogg': '.ogg', 'audio/vorbis': '.ogg'}
+
+# Remove characters that doesn't play well with 'nix and mac filesystems.'
+def sanitize_filename(filename):
+	return filename.replace('/', '-').replace(':', '')
+
+# Small logger.
+def logger(indent, message):
+	if not args.silent:
+		print('\t'*indent + message)
+
+# Get podcast feeds from OPML file or URL
+parsed_opml = opml.parse(args.OPML)
+
+# Setup current working directory.
+if args.dir is not None:
+	os.makedirs(args.dir, exist_ok=True)
+	os.chdir(args.dir)
+download_dir = os.getcwd()
+
+# Delete directory contents if reset option is given.
+if args.reset:
+	for root, dirs, files in os.walk(download_dir):
+		for f in files:
+			os.unlink(os.path.join(root, f))
+		for d in dirs:
+			shutil.rmtree(os.path.join(root, d))
+
+# Set up urllib
+http = urllib3.PoolManager()
+urllib3.disable_warnings()
+
+# Go through each feed
+for index, opml_outline in enumerate(parsed_opml):
+	# Read the RSS Feed from the 'xmlurl' attribute of the opml outline
+	# We go through some trouble to normalize formatting of the attribute keys
+	# since they are case-sensitive *sigh*  -.-
+	opml_outline_keys = opml_outline._root.attrib.keys()
+	for index, key in enumerate(opml_outline_keys):
+		if (key.lower() == 'xmlurl'):
+			rss_feed = feedparser.parse( opml_outline._root.attrib[key] )
+			break
+	
 	#Optionally go through the feed from old to new
 	if args.reverse:
 		rss_feed.entries.reverse()
